488f096cdbd64f15f06b9754a8243e6f330c206b
najif jawoad jim <najifjawoad@gmail.com>
2026-07-09 16:05:47 +0600

    21-1

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -8,36 +8,52 @@ const loginUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
 
-    const {accessToken , refreshToken} = await authService.loginUserIntoDB(payload);
-     
-    res.cookie("accessToken" , accessToken ,{
-      httpOnly : true,
-      secure : false,
-      sameSite : "none",
-      maxAge : 1000 * 60 * 60 * 24
-    })
-    res.cookie("refreshToken" , refreshToken ,{
-      httpOnly : true,
-      secure : false,
-      sameSite : "none",
-      maxAge : 1000 * 60 * 60 * 24 * 7
-    })
+    const { accessToken, refreshToken } =
+      await authService.loginUserIntoDB(payload);
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24,
+    });
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
 
     sendResponse(res, {
       success: true,
       statusCode: httpStatus.OK,
       message: "Logged In successfully",
-      data: {accessToken , refreshToken},
+      data: { accessToken, refreshToken },
     });
   },
 );
 
-const refreshToken = catchAsync( 
-  async(req: Request, res: Response, next: NextFunction)=>{
+const refreshToken = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken;
 
+    const { accessToken } = await authService.refreshToken(refreshToken);
 
-  }
-)
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24,
+    });
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Token Refreshed Successfully",
+      data: { accessToken },
+    });
+  },
+);
 
 export const authController = {
   loginUser,
