66f0df6a3bc4e9340909394e2fa21bb4f76a2341
najif jawoad jim <najifjawoad@gmail.com>
2026-07-11 20:40:39 +0600

    22-3-1

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import { prisma } from "./lib/prisma";
 import bcrypt from "bcryptjs";
 import { userRoutes } from "./modules/users/users.route";
 import { authRoutes } from "./modules/auth/auth.route";
+import { postRoutes } from "./modules/posts/post.router";
+import { commentRoutes } from "./modules/comments/comments.route";
 
 const app: Application = express();
 
@@ -26,5 +28,7 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/api/users" , userRoutes);
 app.use("/api/auth" , authRoutes);
+app.use("/api/posts" , postRoutes);
+app.use("/api/comments", commentRoutes)
 
 export default app;
---
+++ src/modules/comments/comments.controller.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import { catchAsync } from "../../utilities/catchAsync";
+
+const createComment = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    
+})
+
+const getCommentsByAuthorId = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+     
+})
+
+
+const getCommentsByCommentId = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+     
+})
+
+const updateComment = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+     
+})
+
+const deleteComment = catchAsync(async (req: Request, res: Response, next: NextFunction) => {       
+
+})
+
+const moderateComment = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+})
+
+
+
+export const commentController = {
+    createComment,
+    getCommentsByAuthorId,
+    getCommentsByCommentId,
+    updateComment,
+    deleteComment,
+    moderateComment
+}
